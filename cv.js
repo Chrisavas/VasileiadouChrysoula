1186c9d04b9102bacff3296afbb2ed2e28548f78
Chrisa Vasileiadou <76496157+Chrisavas@users.noreply.github.com>
2025-09-29 21:01:58 +0300

    Delete cv.js

--- cv.js
+++
@@ -1,7 +0,0 @@
-
-const contactBtn = document.getElementById("contactBtn");
-const contactOptions = document.getElementById("contactOptions");
-
-contactBtn.addEventListener("click", () => {
-  contactOptions.classList.toggle("show");
-});
