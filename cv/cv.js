e23050e392fc1161ff70164bda7ffb3f510e8d81
Chrisa Vasileiadou <76496157+Chrisavas@users.noreply.github.com>
2025-09-29 21:03:03 +0300

    Add files via upload

---
+++ cv/cv.js
@@ -0,0 +1,7 @@
+
+const contactBtn = document.getElementById("contactBtn");
+const contactOptions = document.getElementById("contactOptions");
+
+contactBtn.addEventListener("click", () => {
+  contactOptions.classList.toggle("show");
+});
